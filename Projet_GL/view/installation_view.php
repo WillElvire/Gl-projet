535f34d7b93f6ac5451a646bdfb8e1b84d968500
WillElivre <willymarley76@gmail.com>
2020-01-11 06:45:10 +0000

    commit

--- Projet_GL/view/installation_view.php
+++ Projet_GL/view/installation_view.php
@@ -9,7 +9,7 @@
             <meta charset="UTF-8">
             
             <?php include_once'includes/partials/getTitle.php'; ?>
-            <link rel="stylesheet" href="assets/css/globalStyle.css">
+            <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
     </head> 
 
@@ -31,13 +31,24 @@
 
                                <div>
                                        
-                                        <form class="form-group" method="post" action="">
+                                        <form class="form-group" method="post" action="" enctype="multipart/data-form">
 
 
                                                 <label>Entrez votre nom</label>
                                                 <input type="text" name="" class="form-control">
                                                 <label>Entrez votre prenom</label>
                                                 <input type="text" name="" class="form-control">
+
+                                                <label>Photo (*faculatitif)</label>
+                                                <div class="box__input">
+                                                <input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
+                                                <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
+                                                 <button class="box__button" type="submit">Upload</button>
+                                                </div>
+                                                <div class="box__uploading">Uploading&hellip;</div>
+                                                <div class="box__success">Done!</div>
+                                                <div class="box__error">Error! <span></span>.</div>
+
                                                 
 
 

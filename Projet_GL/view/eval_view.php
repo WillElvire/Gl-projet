ec392a49c1f2424c57be9c0917f663e88ddefa64
Koua Wilfried elvire <59749493+WillElvire@users.noreply.github.com>
2023-03-30 12:18:42 +0000

    Update eval_view.php

--- Projet_GL/view/eval_view.php
+++ Projet_GL/view/eval_view.php
@@ -1,49 +1,23 @@
 <div class="row">
-
-
-
-<form method="post" class="form-group col-md-12">
-
-
-<label>Entrez l'identifiant de L'Article</label>
-<input type=text name="identifiant" class="form-control border-success">
-<label>Le nom de L'evaluateur</label>
-<input type=text name="evaluateur" class="form-control border-success">
-<label>Numero d'Evaluation</label>
-<select class="form-control border-success" name="evalutation">
-
-	  <option value="1">1</option>
-	  <option value="2">2</option>
-	  <option value="3">3</option>
-	  <option value="4">4</option>
-	
-    
-
-</select>
-<label>Entrez le commentaire</label>
-<input type=text name="commentaire" class="form-control border-success">
-<label>Entrez un Message Pour le President</label>
-<textarea name="message"  rows="7" class="form-control border-success">
-
-
-</textarea>
-
-
-<br>
-<br>
-
-<button class="btn btn-success col-md-12" name="Evaluer">Evaluer</button>
-
-
-</form>
-
-
-
-
-
-
-
-
-
-
+	<form method="post" class="form-group col-md-12">
+		<label>Entrez l'identifiant de L'Article</label>
+		<input type=text name="identifiant" class="form-control border-success">
+		<label>Le nom de L'evaluateur</label>
+		<input type=text name="evaluateur" class="form-control border-success">
+		<label>Numero d'Evaluation</label>
+		<select class="form-control border-success" name="evalutation">
+			  <option value="1">1</option>
+			  <option value="2">2</option>
+			  <option value="3">3</option>
+			  <option value="4">4</option>
+		</select>
+		<label>Entrez le commentaire</label>
+		<input type=text name="commentaire" class="form-control border-success">
+		<label>Entrez un Message Pour le President</label>
+		<textarea name="message"  rows="7" class="form-control border-success">
+		</textarea>
+		<br>
+		<br>
+		<button class="btn btn-success col-md-12" name="Evaluer">Evaluer</button>
+	</form>
 </div>
